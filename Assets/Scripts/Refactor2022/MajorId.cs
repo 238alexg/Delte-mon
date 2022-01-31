9c8c1e92cefa0f705992f3e46b3d2995ca091668
Alex Geoffrey <alexandrgeoffrey@gmail.com>
2022-01-30 18:43:56 -0800

    Add MajorId enum
    Enum of all the majors

---
+++ Assets/Scripts/Refactor2022/MajorId.cs
@@ -0,0 +1,25 @@
+﻿namespace BattleDelts.Data
+{
+    public enum MajorId
+    {
+        Accounting,
+        Business,
+        ComputerScience,
+        Economics,
+        English,
+        EnvironmentalScience,
+        FHS,
+        Geology,
+        History,
+        HumanPhys,
+        IntlStudies,
+        Journalism,
+        Math,
+        NaturalScience,
+        NoMajor,
+        PoliticalScience,
+        ProductDesign,
+        Psychology,
+        Undeclared
+    }
+}
