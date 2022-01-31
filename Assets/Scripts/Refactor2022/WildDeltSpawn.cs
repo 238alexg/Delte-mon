a39f82da8ecde3d2284b48996f575de6fc75cd33
Alex Geoffrey <alexandrgeoffrey@gmail.com>
2022-01-30 20:30:45 -0800

    Create Wild Delt Spawns object and json data
    Pulled from parsing TallGrass scripts from all scenes and compiling groups of the types of delts listed on each one

---
+++ Assets/Scripts/Refactor2022/WildDeltSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BattleDelts.Data
+{
+    [Serializable]
+    public class WildDeltSpawns
+    {
+        public List<MapSpawns> AllSpawns;
+    }
+
+    [Serializable]
+    public class MapSpawns
+    {
+        public string MapName;
+        public List<MapSectionSpawns> Sections;
+    }
+
+    [Serializable]
+    public class MapSectionSpawns
+    {
+        public string SectionName;
+        public List<DeltEncounter> Encounters;
+    }
+
+    [Serializable]
+    public class DeltEncounter
+    {
+        public string DeltName;
+        public string RarityLevel;
+        public int MinLevel;
+        public int MaxLevel;
+    }
+}
