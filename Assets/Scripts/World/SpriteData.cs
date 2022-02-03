7c7c34fd2d4b01bee5c0bdd13a5fa79cbaef6db7
Alex Geoffrey <alexandrgeoffrey@gmail.com>
2022-02-03 00:12:47 -0800

    Add SpriteData class with Delt Sprite dict

---
+++ Assets/Scripts/World/SpriteData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleDelts.Data
+{
+    [Serializable]
+    public class DeltSpriteData
+    {
+        public DeltId DeltId;
+        public Sprite Front;
+        public Sprite Back;
+    }
+
+    public class SpriteData : MonoBehaviour
+    {
+        public Dictionary<DeltId, DeltSpriteData> DeltSprites;
+
+        [SerializeField]
+        private List<DeltSpriteData> DeltSpriteList;
+
+        public void PopulateDictionaries()
+        {
+            DeltSprites = new Dictionary<DeltId, DeltSpriteData>();
+            foreach (var spriteData in DeltSpriteList)
+            {
+                DeltSprites[spriteData.DeltId] = spriteData;
+            }
+        }
+    }
+}
