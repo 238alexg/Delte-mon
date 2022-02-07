709901492ef7138c9da168130710ccb6d32f56f0
Alex Geoffrey <alexandrgeoffrey@gmail.com>
2022-02-07 01:38:37 -0800

    Added input refactor for PC input

---
+++ Assets/Scripts/Refactor2022/Controls/InputEventBroadcaster.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleDelts.Controls
+{
+    public enum InputValue
+    {
+        MoveEast,
+        MoveNorth,
+        MoveWest,
+        MoveSouth,
+
+        AButton,
+        BButton
+    }
+
+    public enum InputState
+    {
+        Down,
+        Pressed,
+        Up
+    }
+
+    public interface IInputGenerator
+    {
+        bool TryGetInputEvents(out Dictionary<InputValue, InputState> inputEvents);
+    }
+
+    public interface IInputConsumer
+    {
+        bool ConsumeInputEvents(Dictionary<InputValue, InputState> inputEvents);
+    }
+
+    public static class InputEventBroadcaster
+    {
+        private static List<IInputGenerator> InputGenerators = new List<IInputGenerator>();
+        private static List<IInputConsumer> InputConsumers = new List<IInputConsumer>();
+        public static Dictionary<InputValue, InputState> InputState = new Dictionary<InputValue, InputState>();
+
+        public static void RegisterInputGenerator(IInputGenerator inputGenerator)
+        {
+            InputGenerators.Add(inputGenerator);
+        }
+
+        public static void RegisterInputConsumer(IInputConsumer inputConsumer)
+        {
+            InputConsumers.Add(inputConsumer);
+        }
+
+        public static void BroadcastInputEvents()
+        {
+            InputState.Clear();
+
+            foreach(var generator in InputGenerators)
+            {
+                if (generator.TryGetInputEvents(out var inputEvents))
+                {
+                    foreach (var inputEvent in inputEvents)
+                    {
+                        if (InputState.ContainsKey(inputEvent.Key))
+                        {
+                            Debug.LogError($"Input State already has input set for: {inputEvent.Key}");
+                        }
+                        else
+                        {
+                            InputState.Add(inputEvent.Key, inputEvent.Value);
+                        }
+                    }
+                }
+            }
+
+            foreach(var consumer in InputConsumers)
+            {
+                if (consumer.ConsumeInputEvents(InputState))
+                {
+                    // TODO: Should I break per input event? This is likely good enough for this game
+                    // Consumer has consumed, stop sending this event
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Refactor2022/Controls/PCInput.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleDelts.Controls
+{
+    public class PCInput : IInputGenerator
+    {
+		private readonly Dictionary<InputValue, InputState> InputEvents = new Dictionary<InputValue, InputState>();
+		private bool AnyInputLastFrame = false;
+
+        public bool TryGetInputEvents(out Dictionary<InputValue, InputState> inputEvents)
+        {
+			InputEvents.Clear();
+
+			bool anyInput = false;
+
+			if (TryGetMovementInput(out var movementInput, out var state))
+            {
+				anyInput = true;
+				InputEvents.Add(movementInput, state);
+			}
+
+			if (TryGetInputStateOfKey(KeyCode.Return, out var aButtonState) ||
+				TryGetInputStateOfKey(KeyCode.Space, out aButtonState))
+            {
+				anyInput = true;
+				InputEvents.Add(InputValue.AButton, aButtonState);
+			}
+
+			if (TryGetInputStateOfKey(KeyCode.LeftShift, out var bButtonState) ||
+				TryGetInputStateOfKey(KeyCode.RightShift, out bButtonState))
+			{
+				anyInput = true;
+				InputEvents.Add(InputValue.BButton, bButtonState);
+			}
+
+			bool anyInputOrInputChange = AnyInputLastFrame || anyInput;
+			AnyInputLastFrame = anyInput;
+
+			inputEvents = InputEvents;
+			return anyInputOrInputChange;
+		}
+
+		private bool TryGetMovementInput(out InputValue inputEvent, out InputState state)
+        {
+			inputEvent = default;
+			if (TryGetInputStateOfKey(KeyCode.W, out state))
+			{
+				inputEvent = InputValue.MoveNorth;
+			}
+			else if (TryGetInputStateOfKey(KeyCode.A, out state))
+			{
+				inputEvent = InputValue.MoveWest;
+			}
+			else if (TryGetInputStateOfKey(KeyCode.S, out state))
+			{
+				inputEvent = InputValue.MoveSouth;
+			}
+			else if (TryGetInputStateOfKey(KeyCode.D, out state))
+			{
+				inputEvent = InputValue.MoveEast;
+			}
+			else
+            {
+				return false;
+            }
+
+			return true;
+		}
+
+		private bool TryGetInputStateOfKey(KeyCode key, out InputState state)
+        {
+			if (Input.GetKeyDown(key))
+            {
+				state = InputState.Down;
+            }
+			else if (Input.GetKeyUp(key))
+            {
+				state = InputState.Up;
+            }
+			else if (Input.GetKey(key))
+            {
+				state = InputState.Pressed;
+            }
+			else
+            {
+				state = default;
+				return false;
+            }
+
+			return true;
+        }
+    }
+}
+
