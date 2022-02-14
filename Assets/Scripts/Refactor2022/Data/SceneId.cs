08d63d3f04350afedb80304fe3dfec027ce6c798
Alex Geoffrey <alexandrgeoffrey@gmail.com>
2022-02-14 00:45:22 -0800

    Add Scene ID enum

---
+++ Assets/Scripts/Refactor2022/Data/SceneId.cs
@@ -0,0 +1,17 @@
+﻿namespace BattleDelts.Data
+{
+    public enum SceneId
+    {
+        Hometown,
+        OnyxSt,
+        ChiTown,
+        Sigston,
+        NineteenthSt,
+        DAGraveyard,
+        Atlambdis,  // Watery -> Full of beer?
+        UniversitySt, // Long st connecting Atlambdis, AEPi and UO
+        AEPiramids, // AEgyptPi? Desert/Egypt/Jewish theme
+        UniversityOfOregon // Metropolis city, many UO buildings/quests
+    }
+}
+
